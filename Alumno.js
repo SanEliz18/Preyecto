15ed221cf468223082652b5c706801654928f223
SanEliz18 <sandra_1322134030@uptecamac.edu.mx>
2023-03-21 01:17:21 -0600

    Clase materias

--- Alumno.js
+++ Alumno.js
@@ -126,65 +126,6 @@ function ost() {
   }
 }
 
-function guardarMateria() {
-  var seleccionados = [];
-
-  var checkboxes = document.querySelectorAll("input[type=checkbox]");
-
-  for (var i = 0; i < checkboxes.length; i++) {
-      if (checkboxes[i].checked) {
-          seleccionados.push(checkboxes[i].value);
-          
-      }
-  }
-  let lista = document.getElementById("lista1");
-  let checks = document.querySelectorAll(".basi"); 
-  let list = document.getElementById("lista2");
-  let checks1 = document.querySelectorAll(".prof"); 
-  let list1 = document.getElementById("lista3");
-  let checks2 = document.querySelectorAll(".met"); 
-  let list2 = document.getElementById("lista4");
-  let checks3 = document.querySelectorAll(".op"); 
-  lista.innerHTML = '';
-      checks.forEach((e)=>{
-          if(e.checked == true){
-               var elemento = document.createElement('li');
-              elemento.className = 'lista1-item';
-              elemento.innerHTML = e.value;
-              lista.appendChild(elemento);   
-          }
-      });
-      list.innerHTML = '';
-      checks1.forEach((e)=>{
-          if(e.checked == true){
-               var elemento = document.createElement('li');
-              elemento.className = 'lista2-item';
-              elemento.innerHTML = e.value;
-              list.appendChild(elemento);   
-          }
-      });
-      list1.innerHTML = '';
-      checks2.forEach((e)=>{
-          if(e.checked == true){
-               var elemento = document.createElement('li');
-              elemento.className = 'lista3-item';
-              elemento.innerHTML = e.value;
-              list1.appendChild(elemento);   
-          }
-      });
-      list2.innerHTML = '';
-      checks3.forEach((e)=>{
-          if(e.checked == true){
-               var elemento = document.createElement('li');
-              elemento.className = 'lista4-item';
-              elemento.innerHTML = e.value;
-              list2.appendChild(elemento);   
-          }
-      });
-
-  localStorage.setItem("seleccionados", JSON.stringify(seleccionados));
-  
-}
 
 function listo(){
   alert("Tu inscripción se llevo a cavo con exito");
